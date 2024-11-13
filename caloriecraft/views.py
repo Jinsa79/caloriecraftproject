6cee87d260fb333591bb826b89148a2cdd89b14c
Jinsa79 <jinsa1979@gmail.com>
2024-11-13 11:17:38 +0900

    first commit

---
+++ caloriecraft/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+# django.views.generic.baseからTemplateViewをインポート
+from django.views.generic.base import TemplateView
+
+class IndexView(TemplateView):
+    '''トップページのビュー
+    
+    テンプレートのレンダリングに特化したTemplateViewを継承
+
+    Attributes:
+        template_name: レンダリングするテンプレート
+    '''
+    # index.htmlをレンダリングする
+    template_name = 'index.html'
