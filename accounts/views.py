15f711829ab539db2b0861cd073ee0e5f48f1474
shinji0116 <151495819+shinji0116@users.noreply.github.com>
2025-02-05 11:53:02 +0900

    Update views.py
    Signed-off-by: shinji0116 <151495819+shinji0116@users.noreply.github.com>

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.views.generic import CreateView, TemplateView
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
 from .forms import UserRegistrationForm
 from django.contrib import messages
  
@@ -18,5 +21,22 @@ def register(request):
 #from django.shortcuts import render
  
 # Create your views here.
+
+class SignUpView(CreateView):
+  form_class = CustomUserCreationForm
+
+  template_name = 'signup.html'
+
+  success_url = reverss_lazy('accounts:signup_success')
+
+  def form_valid(seif, form)
+
+  user = form.save()
+  self.object = user
+
+  return super(). form_valid(form)
+
+class signUpSuccessView(TemplateView):
+ template_name = 'signup_success.html'
  
  
