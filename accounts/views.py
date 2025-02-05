aaaceb7ce9950ec975df0b410664a48e0ac4e477
shinji0116 <151495819+shinji0116@users.noreply.github.com>
2025-02-05 12:22:46 +0900

    Update views.py
    Signed-off-by: shinji0116 <151495819+shinji0116@users.noreply.github.com>

--- accounts/views.py
+++ accounts/views.py
@@ -4,23 +4,6 @@ from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 from .forms import UserRegistrationForm
 from django.contrib import messages
- 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            messages.success(request, '登録が完了しました！')
-            return redirect('login')  # ログインページにリダイレクト
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'accounts/register.html', {'form': form})
- 
-#from django.shortcuts import render
- 
-# Create your views here.
 
 class SignUpView(CreateView):
   form_class = CustomUserCreationForm
